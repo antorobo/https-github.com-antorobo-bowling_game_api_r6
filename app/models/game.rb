d0c38b15b365314d4002ee61838cf8c4e46eb352
antorobo <antonyrobin_amie@yahoo.com>
2020-03-08 15:22:28 +0530

    changed calculation in game.scoreboard

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,7 +5,7 @@ class Game < ApplicationRecord
     Hash.new.tap do |score|
       score[:game_id] = id
       score[:pins] = throws.where(throwId:[1..20]).sum(:pins)
-      score[:bonus_points] = throws.where(throwId:[1..20]).sum(:bonus1) + throws.where(throwId:[1..20]).sum(:bonus2)
+      score[:bonus_points] = throws.where(throwId:[1..20]).sum"(bonus1) + (bonus2)"
       score[:total_score] = score[:pins] + score[:bonus_points]
     end
   end
